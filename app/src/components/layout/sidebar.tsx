4e03f028c38c99cb8bc77167085c421d9accc0b1
mitate-gengaku <mitategengaku@gmail.com>
2025-03-15 23:19:50 +0900

    component: Sidebarコンポーネントを作成 #7

---
+++ app/src/components/layout/sidebar.tsx
@@ -0,0 +1,111 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { AlignLeftIcon, EllipsisIcon } from "lucide-react";
+import React, { useState } from "react";
+import { useDebouncedCallback } from "use-debounce";
+
+import { Button } from "@/components/ui/button";
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const books = ["銀河鉄道の夜", "熊嵐", "高熱街道"]
+
+  const handleSidebar = useDebouncedCallback(
+    (open: React.SetStateAction<boolean>) => {
+      setIsOpen(open);
+    },
+    500,
+  );
+
+  const sidebarVariants = {
+    open: {
+      x: 0,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 30,
+      },
+    },
+    closed: {
+      x: "-100%",
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 30,
+      },
+    },
+  };
+
+  const buttonVariants = {
+    open: {
+      x: 0,
+      rotate: 0,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 30,
+      },
+    },
+    closed: {
+      x: 0,
+      transition: {
+        type: "spring",
+        stiffness: 300,
+        damping: 30,
+      },
+    },
+  };
+
+  return (
+    <>
+      <Button size={"icon"} variant={"ghost"} className="z-[9999]" asChild>
+        <motion.button
+          onClick={() => setIsOpen((open) => !open)}
+          onMouseEnter={() => handleSidebar(true)}
+          onMouseLeave={() => handleSidebar(false)}
+          variants={buttonVariants}
+          animate={isOpen ? "open" : "closed"}
+          aria-label="Toggle sidebar"
+        >
+          <AlignLeftIcon />
+        </motion.button>
+      </Button>
+      <motion.div
+        className="fixed h-[calc(100%-60px)] top-[60px] left-1 bottom-1 w-64 bg-white shadow-lg z-40 rounded-sm"
+        variants={sidebarVariants}
+        initial="closed"
+        animate={isOpen ? "open" : "closed"}
+        onMouseEnter={() => handleSidebar(true)}
+        onMouseLeave={() => handleSidebar(false)}
+      >
+        <div className="h-full pl-3 pr-1 py-6 border-r border-t">
+          <h2 className="text-sm font-bold mb-3">作品一覧</h2>
+          <ScrollArea className="h-[calc(100%-30px)] pr-3">
+            <ul className="space-y-2">
+              {books.map(
+                (item, index) => (
+                  <li key={index} className="relative group">
+                    <a
+                      href={`/book/${item}`}
+                      className="flex pl-2 z-10 pr-5 py-1 text-sm group-hover:bg-teal-50 rounded transition-colors duration-200"
+                    >
+                      {item}
+                    </a>
+                    <button className="z-[999] flex items-center justify-center size-6 absolute right-0 top-0.5 rounded-sm">
+                      <EllipsisIcon className="size-3" />
+                    </button>
+                  </li>
+                ),
+              )}
+            </ul>
+          </ScrollArea>
+        </div>
+      </motion.div>
+    </>
+  );
+};
+
+export default Sidebar;
