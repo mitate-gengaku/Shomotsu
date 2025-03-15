a990b4822c69502a8ef88bb5681065fdd5812b7f
mitate-gengaku <mitategengaku@gmail.com>
2025-03-15 16:41:41 +0900

    chore: remark-gfmをoption経由で渡すとエラーが発生するため、gfm対応は後日行う #5

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,6 +1,5 @@
 import type { NextConfig } from "next";
 import createMDX from '@next/mdx'
-import remarkGfm from 'remark-gfm'
 
 const nextConfig: NextConfig = {
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
@@ -10,9 +9,7 @@ const nextConfig: NextConfig = {
 };
 
 const withMDX = createMDX({
-  options: {
-    remarkPlugins: [remarkGfm]
-  }
+  //
 })
 
 export default withMDX(nextConfig);
