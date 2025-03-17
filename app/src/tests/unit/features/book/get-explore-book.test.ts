aadb65e2f4a90d0308b4ea91c0474a2bfe4d6128
mitate-gengaku <mitategengaku@gmail.com>
2025-03-17 23:32:43 +0900

    test: getExloreBookのテストの作成 #13

---
+++ app/src/tests/unit/features/book/get-explore-book.test.ts
@@ -0,0 +1,30 @@
+import { afterEach } from "node:test";
+
+import { redirect } from "next/navigation";
+import mockRouter from "next-router-mock";
+import { beforeEach } from "vitest";
+import { afterAll, beforeAll, describe, expect, test, vitest } from "vitest";
+
+import { getExploreBooks } from "@/features/book/services/explore-books";
+import { server } from "@/lib/msw/setup/server";
+
+vitest.mock("next/navigation", () => ({
+  redirect: vitest.fn(),
+}));
+
+describe("getExploreBooksのテスト", () => {
+  beforeEach(() => {
+    mockRouter.setCurrentUrl("/");
+  });
+  beforeAll(() => {
+    server.listen({ onUnhandledRequest: "error" });
+  });
+  afterAll(() => server.close());
+  afterEach(() => server.resetHandlers());
+
+  test("本のIDが一致する場合、本のデータが返る", async () => {
+    const book = await getExploreBooks();
+
+    expect(book.length).toBe(10)
+  });
+});
