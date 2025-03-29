b562dc547c45c6e25996e2da208a3bc447600f12
mitate-gengaku <mitategengaku@gmail.com>
2025-03-29 20:50:30 +0900

    add: book, category用のテーブルを追加 #30

--- app/src/components/layout/private-layout.tsx
+++ app/src/components/layout/private-layout.tsx
@@ -9,7 +9,7 @@ export const PrivateLayoutUI = ({
 }: Readonly<{ children: React.ReactNode }>) => {
   return (
     <div
-      className="flex flex-col h-screen bg-white dark:bg-slate-950"
+      className="flex flex-col min-h-screen bg-white dark:bg-slate-950"
       data-testid="auth-layout"
     >
       <AuthHeader>
--- app/src/lib/db/schema/schema.ts
+++ app/src/lib/db/schema/schema.ts
@@ -1,4 +1,12 @@
-import { pgTable, text, timestamp } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import {
+  pgTable,
+  text,
+  timestamp,
+  boolean,
+  varchar,
+} from "drizzle-orm/pg-core";
+import { ulid } from "ulid";
 
 export const usersTable = pgTable("users_table", {
   id: text("id").primaryKey(),
@@ -8,5 +16,64 @@ export const usersTable = pgTable("users_table", {
   createdAt: timestamp("created_at").notNull().defaultNow(),
   updatedAt: timestamp("updated_at")
     .notNull()
+    .defaultNow()
     .$onUpdate(() => new Date()),
 });
+
+export const categoriesTable = pgTable("categories_table", {
+  id: text("id")
+    .$defaultFn(() => ulid())
+    .primaryKey(),
+  category: text("category").notNull().unique(),
+  label: text("label").notNull().unique(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});
+
+
+export const booksTable = pgTable("books_table", {
+  id: text("id")
+    .$defaultFn(() => ulid())
+    .primaryKey(),
+  user_id: text("user_id")
+    .notNull()
+    .references(() => usersTable.id, { onDelete: "cascade", onUpdate: "cascade" }),
+  category_id: text("category_id")
+    .notNull()
+    .references(() => categoriesTable.id, { onDelete: "restrict", onUpdate: "cascade" }),
+  title: varchar({ length: 28 }),
+  description: varchar({ length: 256 }),
+  content: text("content"),
+  cover: text("cover").notNull(),
+  publish: boolean("publish").default(false),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});
+
+/**
+ * relation
+ */
+export const userRelations = relations(usersTable, ({ many }) => ({
+  books: many(booksTable)
+}))
+
+export const bookRelations = relations(booksTable, ({ one }) => ({
+  user: one(usersTable, {
+    fields: [booksTable.user_id],
+    references: [usersTable.id]
+  }),
+  category: one(categoriesTable, {
+    fields: [booksTable.category_id],
+    references: [categoriesTable.id]
+  })
+}))
+
+export const categoryRelations = relations(categoriesTable, ({ many }) => ({
+  books: many(booksTable)
+}))
